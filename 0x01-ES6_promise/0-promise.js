735ece8547ea128b5b5de3a259ccd8746e02609c
dolivon123456 <dolivon555@gmail.com>
2023-04-19 19:40:49 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,12 @@
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		const data = "ALX Promise task";
-		resolve(data);
-	});
+  const success = true;
+
+  // Returns a new Promise with a callback function that takes in a resolve and reject function
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve(); // If success is true, the Promise is resolved with no value
+    } else {
+      reject(); // If success is false, the Promise is rejected with no value
+    }
+  });
 }
