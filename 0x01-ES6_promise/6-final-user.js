3c918d0843f2ccb0d7b4a22fbff0046e67672d80
dolivon123456 <dolivon555@gmail.com>
2023-04-19 22:42:19 +0000

    3 args

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	return Promise.allSettled([
-		signUpUser(firstName, lastName),
-		uploadPhoto(fileName),
-	]).then((values) => {
-		const user = [];
-		for (const item of values) {
-			user.push({ status: item.status, value: item.value || item.reason });
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
     }
-    return user;
+    return arr;
   });
 }
