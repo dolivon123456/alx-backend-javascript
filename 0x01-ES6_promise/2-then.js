44ef5fed4ea34516423b73814a8e59e63ae24a16
dolivon123456 <dolivon555@gmail.com>
2023-04-19 21:11:46 +0000

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,10 @@
 export default function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'success' };
+
   return promise
-      .then(() => ({ status: 200, body: success }))
-      .catch(() => new Error()) // If the Promise rejects, return a new error object
-      .finally(() => console.log(Got a response from the API)); // Log a message indicating that a response was received from the API in the final block
+    .then(() => body)
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
