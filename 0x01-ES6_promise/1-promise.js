fae5970e81d16e1a8d91a99ca036e15fa2254cb1
dolivon123456 <dolivon555@gmail.com>
2023-04-19 20:36:32 +0000

    boolean promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: Success });
+      resolve({
+        status: 200,
+        body: Success,
+      });
     }
     reject(new Error(The fake API is not working currently));
   });
