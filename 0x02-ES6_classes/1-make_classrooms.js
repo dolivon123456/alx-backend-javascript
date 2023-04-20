6bb89f21803e61b18f49d1e86ea4ee8542ffd4a6
dolivon123456 <dolivon555@gmail.com>
2023-04-20 18:00:49 +0000

    return arrays

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-	const classrooms = [
-		new ClassRoom(19),
-		new ClassRoom(20),
-		new ClassRoom(34)
-	];
-	return classrooms;
+  const classrooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
+  return classrooms;
 }
