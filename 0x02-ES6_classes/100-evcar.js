1c36471494c5d744b071bc2b9e16d0899a73fd4b
dolivon123456 <dolivon555@gmail.com>
2023-04-20 21:05:16 +0000

    extends car

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,6 @@
 import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
     this._range = range;
@@ -10,5 +10,3 @@ class EVCar extends Car {
     return Car;
   }
 }
-
-export default EVCar;
