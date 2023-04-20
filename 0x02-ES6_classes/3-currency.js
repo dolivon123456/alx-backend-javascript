f93dc336b83a8312ed06cb78dfb64015dac9925b
dolivon123456 <dolivon555@gmail.com>
2023-04-20 19:21:43 +0000

    method

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,25 +1,21 @@
 export default class Currency {
   constructor(code, name) {
-	  this._code = code;
-	  this._name = name;
+    this.code = code;
+    this.name = name;
   }
-  
-  // getter code
+
   get code() {
     return this._code;
   }
 
-  // setter code
   set code(code) {
     this._code = code;
   }
 
-  // getter name
   get name() {
     return this._name;
   }
 
-  // setter name
   set name(name) {
     this._name = name;
   }
