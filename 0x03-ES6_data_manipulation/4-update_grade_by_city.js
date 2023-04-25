49c9a8c0538f3f42d6a8f1be66269b7bc8a14c1d
dolivon123456 <dolivon555@gmail.com>
2023-04-25 18:49:59 +0000

    update grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,15 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  // Filter the students for the given city
-  const filteredStudents = students.filter(student => student.city === city);
-  
-  // Map the filtered students to update their grades
-  const updatedStudents = filteredStudents.map(student => {
-    const foundGrade = newGrades.find(grade => grade.studentId === student.id);
-    const grade = foundGrade ? foundGrade.grade : "N/A";
-    return { ...student, grade };
-  });
-  
-  return updatedStudents;
+  if (Array.isArray(students) && Array.isArray(newGrades)) {
+    const cityStudents = students.filter((student) => student.location === city);
+    return cityStudents.map((student) => {
+      const newGrade = newGrades.find((g) => g.studentId === student.id);
+      if (newGrade === undefined) {
+        student.grade = 'N/A';
+      } else {
+        student.grade = newGrade.grade;
+      }
+      return student;
+    });
+  }
+  return [];
 }
-
