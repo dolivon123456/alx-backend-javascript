f4ff913aab1091c7cf8f0a51441ec9870f6c935f
dolivon123456 <dolivon555@gmail.com>
2023-04-25 15:51:58 +0000

    filter & map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,15 @@
-
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (Array.isArray(students) && Array.isArray(newGrades)) {
+    const cityStudents = students.filter((student) => student.location === city);
+    return cityStudents.map((student) => {
+      const newGrade = newGrades.find((g) => g.studentId === student.id);
+      if (newGrade === undefined) {
+        student.grade = 'N/A';
+      } else {
+        student.grade = newGrade.grade;
+      }
+      return student;
+    });
+  }
+  return [];
+}
