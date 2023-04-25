d10dbaec97b1f5859a81cd6b14ba2264b0ae022e
dolivon123456 <dolivon555@gmail.com>
2023-04-25 14:14:16 +0000

    get list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@ export default function getListStudents() {
 	const students = [
 		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
 		{ id: 2, firstName: 'James' , location: 'Columbia' },
-		{ id: 5, firstName: 'Serena' , location: 'San Francisco' }
+		{ id: 5, firstName: 'Serena' , location: 'San Francisco' },
 	];
 
 	return students;
