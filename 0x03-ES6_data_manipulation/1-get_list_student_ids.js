4c98b99064ec5da90fb82d9e2f90116c25d0849a
dolivon123456 <dolivon555@gmail.com>
2023-04-25 14:50:36 +0000

    return arrays

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
 export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
+  if (Array.isArray(students)) {
+    return students.map((student) => student.id);
   }
-
-  return students.map((student) => student.id);
+  return [];
 }
 
